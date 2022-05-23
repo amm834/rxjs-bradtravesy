9c23ed1b990a7f49ce4f4eefe6c55fedbf995b27
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-23 18:35:21 +0630

    Event Stream

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-import './style.css'
+import {fromEvent} from "rxjs";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const select = selector => document.querySelector(selector);
+
+const btn = select('#btn')
+const btnStream = fromEvent(btn, 'click')
+btnStream.subscribe(
+    (e) => console.log(e.target.innerHTML),
+    (error) => console.log(error),
+    () => console.log("Complete")
+)
+
+
+const input = select('#input')
+const inputStream = fromEvent(input, 'keyup')
+inputStream.subscribe(
+    (e) => {
+        const text = e.target.value
+        select('#text').innerText = text
+    },
+    (error) => console.log(error),
+    () => console.log("Complete")
+)
+
+const moveStream = fromEvent(document, 'mousemove')
+moveStream.subscribe({
+    next(e) {
+        select('#text').innerText = `x: ${e.clientX}, y: ${e.clientY}`
+    }
+})
